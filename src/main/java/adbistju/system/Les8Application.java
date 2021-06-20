8f0c6dbf3d1f593fcf4972e1c222f7803a456e63
Adbistju <adjytant@gmail.com>
2021-06-21 00:01:44 +0300

    first commit

---
+++ src/main/java/adbistju/system/Les8Application.java
@@ -0,0 +1,13 @@
+package adbistju.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Les8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Les8Application.class, args);
+    }
+
+}
